86aeea5ec380c5e76e5baf4bcada8e92754d7aed
FernandoDCardozoBraga <fer.car.br@gmail.com>
2023-09-23 23:45:58 -0300

    latest edition, the code meets all specifications

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,21 +1,30 @@
 using System;
 
-/**
-class Entry
-{                   tres propiedades: fecha, mensaje aleatorio, respuesta del uruario
-    public DateTime Date { get; set;  }
-    public string Message { get; set; }
-    public string text { get; srt; }
-}
- 
-class Jornal
-
-**/
-class Program
+class JournalEntry //Class to represent a journal entry
 {
-    
-    private static Random _random = new Random();        //objeto Random para generar lista de mensajes aleatorios
-    private static List<string> _messages = new List<string>
+    public DateTime Date { get; set; } //Entry date
+    public string Prompt { get; set; } //Random entry message
+    public string Content { get; set; } //User entry 
+
+    //Constructor to initialize a journal entry
+    public JournalEntry(DateTime date, string prompt, string content)
+    {
+        Date = date;
+        Prompt = prompt;
+        Content = content;
+    }
+
+    public override string ToString()
+    {
+        return $"{Date:yyyy-MM-dd HH:mm:ss}\n{Prompt}\n{Content}\n";
+    }
+}
+
+class Journal
+{
+    private List<JournalEntry> entries = new List<JournalEntry>(); //Entry jornal lists
+    private Random random = new Random();
+    private List<string> prompts = new List<string>  //Random message
     {
         "Who was the most interesting person I interacted with today?",
         "What was the best part of my day?",
@@ -24,14 +33,88 @@ class Program
         "If you had one thing you could do today, what would it be?"
     };
 
+    public void AddEntry(string content) //add entry to the jornal
+    {
+        string prompt = GetRandomPrompt();    //obtaining random message and date
+        DateTime currentDate = DateTime.Now;
+        JournalEntry entry = new JournalEntry(currentDate, prompt, content);
+        entries.Add(entry);  // add the entry to the list
+    }
+
+    public void ViewEntries()  //see all entries
+    {
+        Console.WriteLine("Diary Entries:\n");
+        foreach (var entry in entries)
+        {
+            Console.WriteLine(entry);
+        }
+    }
+
+    public void SaveJournalToFile(string fileName) //save the jornal in a file
+    {
+        File.WriteAllLines(fileName, entries.ConvertAll(entry => entry.ToString()));
+        Console.WriteLine("Journal saved successfully.");
+    }
+
+    public void LoadJournalFromFile(string fileName)  //load the journal from file
+    {
+        if (File.Exists(fileName))
+        {
+            entries.Clear();
+            string[] lines = File.ReadAllLines(fileName);
+
+            for (int i = 0; i < lines.Length; i += 3)   //entry in three dates, question, answer
+            {
+                DateTime date = DateTime.ParseExact(lines[i], "yyyy-MM-dd HH:mm:ss", null);
+                string prompt = lines[i + 1];
+                string content = lines[i + 2];
+                JournalEntry entry = new JournalEntry(date, prompt, content);
+                entries.Add(entry); //add the entry to the jornal
+            }
+
+            Console.WriteLine("Journal loaded successfully.");
+        }
+        else
+        {
+            Console.WriteLine("File not found.");
+        }
+    }
+    //************ method to delete last journal entry
+    public void DeleteLastEntry()
+    {
+        if (entries.Count > 0)
+        {
+            entries.RemoveAt(entries.Count - 1);
+            Console.WriteLine("Last entry deleted.");
+        }
+        else
+        {
+            Console.WriteLine("No entries to delete.");
+        }
+    }
+
+    //********* method to delete all the jornal content
+    public void ClearJournal()
+    {
+        entries.Clear();
+        Console.WriteLine("Journal cleared.");
+    }
+    //*****************
+    private string GetRandomPrompt()    //Private method to get a random message
+    {
+        int randomIndex = random.Next(prompts.Count);
+        return prompts[randomIndex];
+    }
+}
+
+class Program
+{
     static void Main()
     {
-        
-        List<string> entries = new List<string>();     //lista para almacenar las entradas del diario.   (A eliminar)
+        Journal journal = new Journal();
 
         while (true)
         {
-        
             Console.WriteLine("Diary Menu:");
             Console.WriteLine("1. Add an entry");
             Console.WriteLine("2. View entries");
@@ -47,69 +130,51 @@ class Program
             switch (choice)
             {
                 case "1":
-                    
-                    string randomMessage = GetRandomMessage();   //mensaje aleatorio
-                    string currentDate = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
-                    Console.WriteLine($"{currentDate}\n{randomMessage}");
                     Console.Write("Write your diary entry: ");
                     string entry = Console.ReadLine();
-                    
-                    entries.Add($"{currentDate}\n{randomMessage}\n{entry}\n");   //entrada al diario
+                    journal.AddEntry(entry);
                     break;
 
                 case "2":
-                    
-                    Console.WriteLine("Diary Entries:\n");     //entradas almacenadas en el diario.
-                    foreach (var item in entries)
-                    {
-                        Console.WriteLine(item);
-                    }
+                    journal.ViewEntries();
                     break;
 
                 case "3":
-                    
-                    Console.Write("Enter a filename to save the journal: ");    //Solicitud de nombre de archivo
+                    Console.Write("Enter a filename to save the journal: ");
                     string saveFileName = Console.ReadLine();
-                    File.WriteAllLines(saveFileName, entries);    //guarda entradas en archivo.
-                    Console.WriteLine("Journal saved successfully.");
+                    journal.SaveJournalToFile(saveFileName);
                     break;
 
                 case "4":
-                    
-                    Console.Write("Enter a filename to load the journal: ");  //Solicita a usuario nombre de archivo
+                    Console.Write("Enter a filename to load the journal: ");
                     string loadFileName = Console.ReadLine();
-                    if (File.Exists(loadFileName))     //carga la entrada desde archivo si existe.
-                    {
-                        entries = new List<string>(File.ReadAllLines(loadFileName));
-                        Console.WriteLine("Journal loaded successfully.");
-                    }
-                    else
-                    {
-                        Console.WriteLine("File not found.");
-                    }
+                    journal.LoadJournalFromFile(loadFileName);
                     break;
 
                 case "5":
-                    
-                    if (entries.Count > 0)
-                    {
-                        entries.RemoveAt(entries.Count - 1);  //Elimina la última entrada (Previo a opción 3)
-                        Console.WriteLine("Last entry deleted.");
-                    }
-                    else
-                    {
-                        Console.WriteLine("No entries to delete.");
-                    }
+                    journal.DeleteLastEntry();
                     break;
 
                 case "6":
-                    
-                    entries.Clear();    //Borramos todas las entradas  INCOMPLETO
-                    Console.WriteLine("Journal cleared.");
+                    Console.WriteLine("WARNING: Deleting the entire journal will erase all stored memories.");
+                    Console.Write("Are you sure you want to proceed? (Yes/No): ");
+                    string confirmation = Console.ReadLine().Trim().ToLower();
+
+                    if (confirmation == "yes")
+                    {
+                        journal.ClearJournal();
+                    }
+                    else if (confirmation == "no")
+                    {
+                        Console.WriteLine("Operation canceled.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input. Please enter 'Yes' or 'No'.");
+                    }
                     break;
 
                 case "7":
-                                            //Salimos del programa.
                     Environment.Exit(0);
                     break;
 
@@ -118,12 +183,5 @@ class Program
                     break;
             }
         }
-    }
-
-    
-    private static string GetRandomMessage()   //Obtención de mensaje aleatorio
-    {
-        int randomIndex = _random.Next(_messages.Count);
-        return _messages[randomIndex];
     }
 }
