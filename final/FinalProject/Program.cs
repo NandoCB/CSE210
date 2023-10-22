60528b05cbdba773cf844b01c16b0c7cd17477d8
FernandoDCardozoBraga <fer.car.br@gmail.com>
2023-10-21 22:02:44 -0300

    w7-FP-Finished

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -9,8 +9,9 @@ class Program
     }
 }
 
-abstract class Operation
+abstract class Operation // Abstract class to define a generic operation.
 {
+    //Concrete classes that inherit from Operation and provide specific implementations for each operation.
     public abstract double Execute(double x, double y);
 }
 
@@ -44,7 +45,7 @@ class Division : Operation
     {
         if (y == 0)
         {
-            Console.WriteLine("Error: No se puede dividir por cero.");
+            Console.WriteLine("Error: Cannot divide by zero.");
             return 0;
         }
         return x / y;
@@ -71,6 +72,7 @@ class Calculator
 {
     private Operation[] operations;
 
+    //Initializes an array of operations with the concrete classes of operations.
     public Calculator()
     {
         operations = new Operation[]
@@ -89,35 +91,36 @@ class Calculator
         while (true)
         {
             Console.WriteLine("CSE210 - Calculator:");
-            Console.WriteLine("1. Suma");
-            Console.WriteLine("2. Resta");
-            Console.WriteLine("3. Multiplicación");
-            Console.WriteLine("4. División");
-            Console.WriteLine("5. Potencia");
-            Console.WriteLine("6. Raíz");
-            Console.WriteLine("0. Salir");
+            Console.WriteLine("1. Sum");
+            Console.WriteLine("2. Subtraction");
+            Console.WriteLine("3. Multiplication");
+            Console.WriteLine("4. Division");
+            Console.WriteLine("5. Power");
+            Console.WriteLine("6. Root");
+            Console.WriteLine("0. Exit");
 
-            Console.Write("Elija una opción: ");
+            Console.Write("Choose an option: ");
             int choice = int.Parse(Console.ReadLine());
 
-            if (choice == 0)
+            if (choice == 0)    //Exit the loop if option 0 is chosen.
             {
                 break;
             }
 
             if (choice >= 1 && choice <= 6)
             {
-                Console.Write("Ingrese el primer número: ");
+                Console.Write("Enter the first number [addend, minuend, multiplicand, divident, base, radicand]: ");
                 double x = double.Parse(Console.ReadLine());
-                Console.Write("Ingrese el segundo número: ");
+                Console.Write("Enter the second number [addend, subtrahend, multiplicator, divident, exponent, index]: ");
                 double y = double.Parse(Console.ReadLine());
 
+                            // Executes the selected operation and displays the result.
                 double result = operations[choice - 1].Execute(x, y);
                 Console.WriteLine($"Resultado: {result}");
             }
             else
             {
-                Console.WriteLine("Opción no válida. Intente de nuevo.");
+                Console.WriteLine("Invalid option. Try again.");
             }
         }
     }
